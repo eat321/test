07a3b3432ae203147a76f2d4d073e786ddc0360a
강성재 <gorajrwk@gmail.com>
2025-07-30 17:07:02 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("테스드 작업!!");
+Console.WriteLine(DateTime.Now.Hour < 3 || 8 > DateTime.Now.Hour);
+Console.WriteLine(3 < DateTime.Now.Hour && DateTime.Now.Hour <  8);
+
+
+
+//안 
+//녛
+//하
+//Console.WriteLine("true");
+//Console.WriteLine("false");
